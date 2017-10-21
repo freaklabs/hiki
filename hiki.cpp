b765d6982a0c962ce032fbcb4681a4f55ceb427f
freaklabs <akiba@freaklabs.org>
2017-10-21 09:43:55 +0900

    Implemented functionality
    Implemented most of the methods, tweaked method names, and tested out in
    sketch.

--- hiki.cpp
+++ hiki.cpp
@@ -1,6 +1,13 @@
 #include "hiki.h"
 
+#define LCD_ADDRESS 0x27
+#define DHTPIN  4
+#define DHTTYPE DHT11
+
 Hiki hiki;
+
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+DHT dht(DHTPIN, DHTTYPE);
 
 /*************************************************************
 
@@ -15,6 +22,10 @@ Hiki::Hiki()
 *************************************************************/
 void Hiki::begin()
 {
+    startTime = millis();
+    lcd.begin();
+    rtc.begin();
+    dht.begin();
 	for (int i=0; i<NUM_CHANNELS; i++)
 	{
 		pinMode(chan[i], OUTPUT);
@@ -25,9 +36,12 @@ void Hiki::begin()
 /*************************************************************
 
 *************************************************************/
-void Hiki::dim(uint8_t channel, uint8_t val)
+void Hiki::write(uint8_t channel, uint8_t val)
 {
-
+    if (channel < NUM_CHANNELS)
+    {
+        analogWrite(chan[channel], val);
+    }
 }
 
 /*************************************************************
@@ -35,77 +49,209 @@ void Hiki::dim(uint8_t channel, uint8_t val)
 *************************************************************/
 void Hiki::on(uint8_t channel)
 {
-
+    if (channel < NUM_CHANNELS)
+    {
+        analogWrite(chan[channel], 255);
+    }
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::off(uint8_t channel
+void Hiki::off(uint8_t channel)
 {
-
+    if (channel < NUM_CHANNELS)
+    {
+        analogWrite(chan[channel], 0);
+    }
 }
 
 /*************************************************************
 
 *************************************************************/
-int Hiki::read(uint8_t knob)
+uint8_t Hiki::read(uint8_t knob)
 {
-
+    uint16_t val = analogRead(knob);
+    val = ~val;
+    return (val >> 2);
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::lcdPrint(char *msg)
+void Hiki::print(const char *msg)
 {
-
+    lcd.print(msg);
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::lcdSetCursor(uint8_t col, uint8_t row)
+void Hiki::print(uint8_t val)
 {
-
+    lcd.print(val);
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::lcdClear()
+void Hiki::print(float val)
 {
-
+    lcd.print(val);
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::setTime(uint8_t hour, uint8_t minute, uint8_t second)
+void Hiki::print(char val)
 {
-
+    lcd.print(val);
 }
 
 /*************************************************************
 
 *************************************************************/
-void Hiki::setdate(uint16_t year, uint8_t month, uint8_t day)
+void Hiki::setCursor(uint8_t col, uint8_t row)
 {
-
+    lcd.setCursor(col, row);
 }
 
 /*************************************************************
 
 *************************************************************/
-char *Hiki::readTime()
+void Hiki::clear()
 {
-
+    lcd.clear();
 }
 
 /*************************************************************
 
 *************************************************************/
-char *Hiki::readDate()
+void Hiki::setTime(uint8_t hour, uint8_t min, uint8_t sec)
 {
+    rtc.getTime(&time);
+    time.hour = hour;
+    time.min = min;
+    time.sec = sec;
+    rtc.setTime(&time);
+}
 
+/*************************************************************
+
+*************************************************************/
+void Hiki::setDate(uint16_t year, uint8_t mon, uint8_t day)
+{
+    rtc.getTime(&time);
+    time.year = year;
+    time.mon = mon;
+    time.mday = day;
+    rtc.setTime(&time);
+}
+
+/*************************************************************
+
+*************************************************************/
+char *Hiki::getTime()
+{
+    rtc.getTime(&time);
+    sprintf(buf, "%02d:%02d:%02d", time.hour, time.min, time.sec);
+    return buf;
+}/*************************************************************
+
+*************************************************************/
+char *Hiki::getDateShort()
+{
+    rtc.getTime(&time);
+    sprintf(buf, "%02d/%02d", time.mon, time.mday);
+    return buf;
+}
+
+/*************************************************************
+
+*************************************************************/
+char *Hiki::getDateFull()
+{
+    rtc.getTime(&time);
+    sprintf(buf, "%04d/%02d/%02d", time.year, time.mon, time.mday);
+    return buf;
+}
+
+/*************************************************************
+
+*************************************************************/
+char *Hiki::getTimeAndDate()
+{
+    rtc.getTime(&time);
+    sprintf(buf, "%02d/%02d/%02d %02d:%02d:%02d", time.year, time.mon, time.mday, time.hour, time.min, time.sec);
+    return buf;
+}
+
+/*************************************************************
+
+*************************************************************/
+float Hiki::temperature()
+{
+    return dht.readTemperature();
+}
+
+/*************************************************************
+
+*************************************************************/
+char *Hiki::getTemp()
+{
+    uint8_t val = dht.readTemperature();
+    sprintf(buf, "%02d", val);
+    return buf; 
+}
+
+/*************************************************************
+
+*************************************************************/
+float Hiki::humidity()
+{
+    return dht.readHumidity();
+}
+
+/*************************************************************
+
+*************************************************************/
+char *Hiki::getHum()
+{
+    uint8_t val = dht.readHumidity();
+    sprintf(buf, "%02d", val);
+    return buf; 
+}
+
+/************************************************************************/
+// elapsedTime - calculates time elapsed from startTime
+// startTime : time to start calculating
+/************************************************************************/
+bool Hiki::wait(uint32_t delay)
+{
+  uint32_t stopTime = millis();
+  
+  if (stopTime >= startTime)
+  {
+    if ((stopTime - startTime) > delay)
+    {
+        startTime = millis();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+  }
+  else
+  {
+    if ((ULONG_MAX - (startTime - stopTime)) > delay)
+    {
+        startTime = millis();
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+  }
 }
--- hiki.h
+++ hiki.h
@@ -1,27 +1,50 @@
 #pragma once
-#include "Arduino.h"
 
+#include "LiquidCrystal_I2C.h"
+#include "Arduino.h"
+#include "DHT.h"
+#include "rtc3231.h"
+#include <limits.h>
+
+enum
+{
+	NUM_CHANNELS = 4,
+	NUM_KNOBS = 2
+};
 
 class Hiki
 {
 public:
-	byte knobPin[] = {A0, A1};
-	byte chan[] = {0, 1, 2, 3};
-	byte dhtPin = 4;
+	byte knobPin[2] = {14, 15};
+	byte chan[4] = {9, 10, 5, 6};
+	ts_t time;
+	char buf[50];
+	uint32_t startTime;
+
 
 	Hiki();
 	void begin();
-	void dim(uint8_t, uint8_t);
+	void write(uint8_t, uint8_t);
 	void on(uint8_t);
 	void off(uint8_t);
-	int read(uint8_t);
-	void lcdPrint(char *);
-	void lcdSetCursor(uint8_t, uint8_t);
-	void lcdClear();
+	uint8_t read(uint8_t);
+	void print(char);
+	void print(const char *);
+	void print(uint8_t );
+	void print(float );
+	void setCursor(uint8_t, uint8_t);
+	void clear();
 	void setTime(uint8_t, uint8_t, uint8_t);
-	void setdate(uint16_t, uint8_t, uint8_t);
-	char *readTime();
-	char *readDate();
+	void setDate(uint16_t, uint8_t, uint8_t);
+	char *getTime();
+	char *getDateShort();
+	char *getDateFull();
+	char *getTimeAndDate(); 
+	char *getTemp();
+	char *getHum();
+	float temperature();
+	float humidity();
+	bool wait(uint32_t startTime);
 //	void setAlarm();
 //	void getAlarm();
 
